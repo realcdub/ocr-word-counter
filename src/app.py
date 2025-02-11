c23fa02b8dc7519cfb6146af4f96eeb3706e8a53
realcdub <chwh1021@gmail.com>
2025-02-11 11:45:14 -0500

    revert

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from google import genai
 from PIL import Image
 
 app = Flask(__name__)
-client = genai.Client(api_key="AIzaSyALE-wtnq-Qb4vNZ1rkqJYEoKYXmhnNEqo")
+client = genai.Client(api_key="GEMINI API KEY HERE")
 
 @app.route("/")
 def main():
@@ -14,6 +14,7 @@ def main():
 
 @app.route("/processImage", methods=['POST'])
 def processImage():
+   print("PROCESSING")
    image = Image.open(request.files["file"].stream);
    response = client.models.generate_content(
        model="gemini-2.0-flash",
